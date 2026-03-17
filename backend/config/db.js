0c6dda6c1fb36bc1c763682e977681531e6251ea
Enis Ozgur <enis.destek@gmail.com>
2026-03-17 05:43:07 +0300

    updated db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,7 +12,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { 
+    rejectUnauthorized: false 
+  }
 });
 
 module.exports = pool;
