dab4b7d9dcc0e458067291be0549afecaa2ee8d4
Enis Ozgur <enis.destek@gmail.com>
2026-03-18 18:33:52 +0300

    db settings changed for prod

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 // New requests wait in a queue and are processed one by one. 
 // This prevents overload and keeps the system stable.
 
+//? dev env
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -16,5 +18,15 @@ const pool = new Pool({
     rejectUnauthorized: false 
   }
 });
+*/
+
+//? prod env
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL_INTERNAL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: { 
+    rejectUnauthorized: false 
+  }
+});
+
 
 module.exports = pool;
