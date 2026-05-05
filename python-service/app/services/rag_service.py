6b0ce749cb4e940ec3e91dacde314ec33ecc3eac
Enis Ozgur <enis.destek@gmail.com>
2026-05-05 12:44:15 +0300

    llm model changed and rag_service edited

--- python-service/app/services/rag_service.py
+++ python-service/app/services/rag_service.py
@@ -3,8 +3,8 @@ from groq import Groq
 
 client = Groq(api_key=os.environ["GROQ_API_KEY"])
 
-SYSTEM = """Sen {brand_name} adlı giyim mağazasının Türkçe müşteri hizmetleri asistanısın. Lojistik, Teknik Destek(web sitesi ve mobil için), Finans ve Müşteri hizmetleri departmanlarımız var. 
-Aşağıdaki SSS bilgilerini kullanarak kullanıcıya sıcak ve doğal bir dille yardım et.
+SYSTEM = """Sen {brand_name} adlı  kurumsal bir giyim markasının Türkçe müşteri hizmetleri asistanısın. Lojistik, Teknik Destek(web sitesi ve mobil için), Finans ve Müşteri hizmetleri departmanlarımız var. 
+Aşağıdaki SSS bilgilerini kullanarak kullanıcıya sıcak ve doğal bir dille yardım et. Konuşma üslubun  çözüm üretici olsun. Kullanıcıyı sıkma ve ona güven ver.
 SSS'de cevap yoksa dürüstçe söyle, müşteri hizmetlerine yönlendir.
 Önceki mesajları hatırla. Kullanıcıya "siz" diye hitap et. Sadece Türkçe yanıt ver.
 
@@ -13,13 +13,13 @@ SSS:
 
 def chat(brand_name: str, message: str, faq_results: list[dict], history: list[dict]) -> str:
     context = "\n".join(                                                  # take faq results and join them (3 results returned)
-        f"[{r['department']}] {r['response']}" for r in faq_results
+        f"[{r['department']}] {r['response']}" for r in faq_results       # JSON to prompt formatting
     ) or "Bu konuda Sıkça Sorulan Sorular'da bilgi yok."    
 
     messages = history[-10:] + [{"role": "user", "content": message}]   # last 10 messages should be 
 
     res = client.chat.completions.create(                               # request to groq
-        model="llama-3.3-70b-versatile",
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
         messages=[
             {"role": "system", "content": SYSTEM.format(brand_name=brand_name, context=context)},
             *messages
