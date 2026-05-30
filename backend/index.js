07a31772cb59f84da1adf6ced0232381bc57148d
Enis ÖZGÜR <enis.destek@gmail.com>
2026-05-30 18:30:09 +0300

    test3

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-//test for new acc
+//test for new acct
 app.use(cors(corsOptions));             // Enable requests for all routes
 app.use(bodyParser.json());     // Parse JSON bodies for all routes
 
