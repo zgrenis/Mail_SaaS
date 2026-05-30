89dfd062fb0b21fbbbdea117c638ce865e6848f2
Enis ÖZGÜR <enis.destek@gmail.com>
2026-05-30 18:24:33 +0300

    test

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,19 @@ const cors = require('cors');
 require('dotenv').config();
 
 
-const app = express();          
+const app = express();
 const corsOptions = {
   // .env'den gelen değerin sonunda slash varsa onu temizleyelim (garantiye alalım)
-  origin: process.env.FRONTEND_URL?.replace(/\/$/, ""), 
+  origin: process.env.FRONTEND_URL?.replace(/\/$/, ""),
   credentials: true,
-  optionsSuccessStatus: 200 
+  optionsSuccessStatus: 200
 };
+
 
 app.use(cors(corsOptions));             // Enable requests for all routes
 app.use(bodyParser.json());     // Parse JSON bodies for all routes
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const userRoutes = require('./routes/user.routes');     //import user routes
 app.use('/api/users', userRoutes);                      // Use user routes with /api/users prefix
@@ -34,10 +35,10 @@ const { startMailPoller } = require('./jobs/mailPoller');  // Start the mail pol
 startMailPoller();           // Start the mail poller cron job to check for new emails every 5 minutes
 
 
-const PORT = process.env.PORT || 5000;  
-const HOST = process.env.HOST || '0.0.0.0'; 
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
-    console.log(`🚀 Sunucu başarıyla başlatıldı!`);
-    console.log(`🌍 Adres: http://${HOST}:${PORT}`);
+  console.log(`🚀 Sunucu başarıyla başlatıldı!`);
+  console.log(`🌍 Adres: http://${HOST}:${PORT}`);
 });  // Start the server and listen on the specified port
